bfabe5c6de31f5fcd4efc82daa5875cb3d25dab6
tran thanh tu <tttuab@gmail.com>
2022-11-29 11:41:46 +0700

    update

--- src/Redux/Actions/productActions.js
+++ src/Redux/Actions/productActions.js
@@ -59,7 +59,7 @@ export const getProductDetail = (id) => async (dispatch) => {
       type: PRODUCT_DETAIL_REQUEST,
     });
 
-    const { data } = await axios.get(`${apiUrl}/product/${id}`);
+    const { data } = await axios.get(`${apiUrl}/api/v1/product/${id}`);
 
     dispatch({
       type: PRODUCT_DETAIL_SUCCESS,
@@ -77,7 +77,7 @@ export const getAdminProducts = () => async (dispatch) => {
   try {
     dispatch({ type: ADMIN_PRODUCTS_REQUEST });
 
-    const { data } = await axios.get(`${apiUrl}/admin/products`);
+    const { data } = await axios.get(`${apiUrl}/api/v1/admin/products`);
 
     dispatch({
       type: ADMIN_PRODUCTS_SUCCESS,
@@ -102,7 +102,7 @@ export const deleteProduct = (id) => async (dispatch) => {
   try {
     dispatch({ type: DELETE_PRODUCT_REQUEST });
 
-    const { data } = await axios.delete(`${apiUrl}/admin/product/${id}`);
+    const { data } = await axios.delete(`${apiUrl}/api/v1/admin/product/${id}`);
 
     dispatch({
       type: DELETE_PRODUCT_SUCCESS,
@@ -128,7 +128,7 @@ export const updateProduct = (id, productData) => async (dispatch) => {
     };
 
     const { data } = await axios.put(
-      `${apiUrl}/admin/product/${id}`,
+      `${apiUrl}/api/v1/admin/product/${id}`,
       productData,
       config
     );
@@ -156,7 +156,7 @@ export const newProduct = (productData) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      `${apiUrl}/admin/product/new`,
+      `${apiUrl}/api/v1/admin/product/new`,
       productData,
       config
     );

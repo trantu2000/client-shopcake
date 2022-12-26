d3adcefaa570bbbdbe36b8bdbe7f74de58e42687
tran thanh tu <tttuab@gmail.com>
2022-12-26 22:16:33 +0700

    update

--- src/Redux/Actions/userActions.js
+++ src/Redux/Actions/userActions.js
@@ -1,123 +1,119 @@
-import { 
-    ALL_USERS_FAIL,
-    ALL_USERS_REQUEST,
-    ALL_USERS_SUCCESS,
-    CLEAR_ERRORS,
-    LOAD_USER_FAIL,
-    LOAD_USER_REQUEST,
-    LOAD_USER_SUCCESS,
-    LOGIN_FAIL, 
-    LOGIN_REQUEST, 
-    LOGIN_SUCCESS, 
-    LOGOUT_FAIL, 
-    LOGOUT_SUCCESS, 
-    REGISTER_USER_FAIL, 
-    REGISTER_USER_REQUEST,
-    REGISTER_USER_SUCCESS,
-    UPDATE_PASSWORD_FAIL,
-    UPDATE_PASSWORD_REQUEST,
-    UPDATE_PASSWORD_SUCCESS,
-    UPDATE_PROFILE_FAIL,
-    UPDATE_PROFILE_REQUEST,
-    UPDATE_PROFILE_SUCCESS,
-    UPDATE_USER_FAIL,
-    UPDATE_USER_REQUEST,
-    UPDATE_USER_SUCCESS,
-    USER_DETAILS_FAIL,
-    USER_DETAILS_REQUEST,
-    USER_DETAILS_SUCCESS
-} from "../Constants/userConstants"
-import axios from "axios"
-import { apiUrl } from "../Constants/apiUrl"
-import setAuthToken from "../../utils/setAuthToken"
-
+import {
+  ALL_USERS_FAIL,
+  ALL_USERS_REQUEST,
+  ALL_USERS_SUCCESS,
+  CLEAR_ERRORS,
+  LOAD_USER_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_USER_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  UPDATE_PASSWORD_FAIL,
+  UPDATE_PASSWORD_REQUEST,
+  UPDATE_PASSWORD_SUCCESS,
+  UPDATE_PROFILE_FAIL,
+  UPDATE_PROFILE_REQUEST,
+  UPDATE_PROFILE_SUCCESS,
+  UPDATE_USER_FAIL,
+  UPDATE_USER_REQUEST,
+  UPDATE_USER_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+} from "../Constants/userConstants";
+import axios from "axios";
+import { apiUrl } from "../Constants/apiUrl";
+import setAuthToken from "../../utils/setAuthToken";
 
 // Register user
 export const register = (userData) => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
 
-        dispatch({ type: REGISTER_USER_REQUEST })
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const { data } = await axios.post(`${apiUrl}/register`, userData, config);
+    localStorage.setItem("shop-cake-tu", data.token);
+    await loadUser();
 
-        const { data } = await axios.post(`${apiUrl}/register`, userData, config)
-        localStorage.setItem('shop-cake-tu', data.token)
-        await loadUser()
-
-        dispatch({
-            type: REGISTER_USER_SUCCESS,
-            payload: data.user
-        })
-
-    } catch (error) {
-        dispatch({
-            type: REGISTER_USER_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    dispatch({
+      type: REGISTER_USER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Load user
 export const loadUser = () => async (dispatch) => {
-    if (localStorage['shop-cake-tu']) {
-        setAuthToken(localStorage['shop-cake-tu'])
-    }
-    try {
+  if (localStorage["shop-cake-tu"]) {
+    setAuthToken(localStorage["shop-cake-tu"]);
+  }
+  try {
+    dispatch({ type: LOAD_USER_REQUEST });
 
-        dispatch({ type: LOAD_USER_REQUEST })
+    const { data } = await axios.get(`${apiUrl}/me`);
 
-        const { data } = await axios.get(`${apiUrl}/me`)
-
-        dispatch({
-            type: LOAD_USER_SUCCESS,
-            payload: data.user
-        })
-
-    } catch (error) {
-        dispatch({
-            type: LOAD_USER_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    dispatch({
+      type: LOAD_USER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOAD_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 // Login
 export const login = (email, password) => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: LOGIN_REQUEST });
 
-        dispatch({ type: LOGIN_REQUEST })
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const { data } = await axios.post(
+      `${apiUrl}/login`,
+      { email, password },
+      config
+    );
 
-        const { data } = await axios.post(`${apiUrl}/login`, { email, password }, config)
-    
-        localStorage.setItem('shop-cake-tu', data.token)
-        await loadUser()
-        dispatch({
-            type: LOGIN_SUCCESS,
-            payload: data.user
-        })
-        
-        
-    } catch (error) {
-        dispatch({
-            type: LOGIN_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    localStorage.setItem("shop-cake-tu", data.token);
+    await loadUser();
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
-
-
-// Logout user
+//Logout user
 export const logout = () => async (dispatch) => {
+    localStorage.removeItem("shop-cake-tu");
+  setAuthToken(null);
     try {
 
         await axios.get(`${apiUrl}/logout`)
@@ -134,135 +130,131 @@ export const logout = () => async (dispatch) => {
     }
 }
 
+
+
 // Get all users (admin)
 export const allUsers = () => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: ALL_USERS_REQUEST });
 
-        dispatch({ type: ALL_USERS_REQUEST })
+    const { data } = await axios.get(`${apiUrl}/admin/users`);
 
-        const { data } = await axios.get(`${apiUrl}/admin/users`)
-
-
-        dispatch({
-            type: ALL_USERS_SUCCESS,
-            payload: data.users
-        })
-
-    } catch (error) {
-        dispatch({
-            type: ALL_USERS_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    dispatch({
+      type: ALL_USERS_SUCCESS,
+      payload: data.users,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_USERS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Update user - ADMIN
 export const updateUser = (id, userData) => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: UPDATE_USER_REQUEST });
 
-        dispatch({ type: UPDATE_USER_REQUEST })
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const { data } = await axios.put(
+      `${apiUrl}/admin/user/${id}`,
+      userData,
+      config
+    );
 
-        const { data } = await axios.put(`${apiUrl}/admin/user/${id}`, userData, config)
-
-        dispatch({
-            type: UPDATE_USER_SUCCESS,
-            payload: data.success
-        })
-
-    } catch (error) {
-        dispatch({
-            type: UPDATE_USER_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    dispatch({
+      type: UPDATE_USER_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 // Get user details - ADMIN
 export const getUserDetails = (id) => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: USER_DETAILS_REQUEST });
 
-        dispatch({ type: USER_DETAILS_REQUEST })
+    const { data } = await axios.get(`${apiUrl}/admin/user/${id}`);
 
-
-        const { data } = await axios.get(`${apiUrl}/admin/user/${id}`)
-
-        dispatch({
-            type: USER_DETAILS_SUCCESS,
-            payload: data.user
-        })
-
-    } catch (error) {
-        dispatch({
-            type: USER_DETAILS_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
-
-
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Clear Errors
 export const clearErrors = () => async (dispatch) => {
-    dispatch({
-        type: CLEAR_ERRORS
-    })
-}
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
 
 // Update profile
 export const updateProfile = (userData) => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: UPDATE_PROFILE_REQUEST });
 
-        dispatch({ type: UPDATE_PROFILE_REQUEST })
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const { data } = await axios.put(`${apiUrl}/me/update`, userData, config);
 
-        const { data } = await axios.put(`${apiUrl}/me/update`, userData, config)
-
-        dispatch({
-            type: UPDATE_PROFILE_SUCCESS,
-            payload: data.success
-        })
-
-    } catch (error) {
-        dispatch({
-            type: UPDATE_PROFILE_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    dispatch({
+      type: UPDATE_PROFILE_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PROFILE_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Update password
 export const updatePassword = (passwords) => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: UPDATE_PASSWORD_REQUEST });
 
-        dispatch({ type: UPDATE_PASSWORD_REQUEST})
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const { data } = await axios.put(
+      `${apiUrl}/password/update`,
+      passwords,
+      config
+    );
 
-        const { data } = await axios.put(`${apiUrl}/password/update`, passwords, config)
-
-        dispatch({
-            type: UPDATE_PASSWORD_SUCCESS,
-            payload: data.success
-        })
-
-    } catch (error) {
-        dispatch({
-            type: UPDATE_PASSWORD_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    dispatch({
+      type: UPDATE_PASSWORD_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
